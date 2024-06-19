efb5de8d4c1c0da7f1e3e4e591b165e4e06fdc45
zoro <1rn21ai067.lakshmish@gmail.com>
2024-06-19 16:42:52 +0530

    add comments

--- add two number.py
+++ add two number.py
@@ -1,3 +1,4 @@
+#Addition code in python
 a =int(input("Enter number 1: "))
 b=int(input("Enter number 2: "))
 print("Addition: ",a+b)
--- mulitplication.py
+++ mulitplication.py
@@ -1,3 +1,4 @@
+#addition code in python
 a =int(input("Enter number 1: "))
 b=int(input("Enter number 2: "))
 print("Mutiplication: ",a*b)
