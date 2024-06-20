d8152eb861e040ded9289a7c68eb7fedf00e8998
zoro <1rn21ai067.lakshmish@gmail.com>
2024-06-20 07:51:17 +0530

    c programming

--- add two number.py
+++
@@ -1,4 +0,0 @@
-#Addition code in python
-a =int(input("Enter number 1: "))
-b=int(input("Enter number 2: "))
-print("Addition: ",a+b)
--- mulitplication.py
+++
@@ -1,4 +0,0 @@
-#addition code in python
-a =int(input("Enter number 1: "))
-b=int(input("Enter number 2: "))
-print("Mutiplication: ",a*b)
